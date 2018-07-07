9e21416cc943c12d2d42e2f3b912468592524ad4
Jeffrey <zhide.mo@gmail.com>
2018-07-07 17:20:04 -0400

    Update calculator controller

--- src/main/java/com/github/jeffrey/spring/boot/controller/Big2CalcController.java
+++ src/main/java/com/github/jeffrey/spring/boot/controller/Big2CalcController.java
@@ -4,18 +4,17 @@ import com.github.jeffrey.spring.boot.model.big2.game.Big2Game;
 import com.github.jeffrey.spring.boot.model.big2.game.repository.Big2GameRepository;
 import com.github.jeffrey.spring.boot.model.big2.game.repository.PlayerRepository;
 import com.github.jeffrey.spring.boot.engine.Big2Calculator;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller; // Thymeleaf
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController; // Spring RESTful
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@RestController
-@Component
+@Controller
 public class Big2CalcController {
     private PlayerRepository playerRepository;
     private Big2GameRepository big2GameRepository;
@@ -26,11 +25,12 @@ public class Big2CalcController {
     }
 
     @RequestMapping("/big2calc")
-    public Big2Game calculateBig2GameResult(@RequestParam(name="numbers", required=true) String nums, Model model) {
+    public String calculateBig2GameResult(@RequestParam(name="numbers", required=true) String nums, Model model) {
         List<Integer> integers = Arrays.asList(nums.split(",")).stream().map(Integer::parseInt).collect(Collectors.toList());
         Big2Game result = new Big2Calculator().calculateBig2GainOrLoss(integers);
         result.getPlayers().forEach(p -> playerRepository.save(p));
         big2GameRepository.save(result);
-        return result;
+        model.addAttribute("players", playerRepository.findAll());
+        return "big2calc";
     }
 }
